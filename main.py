eefb9b7c1b9ffa8757a60934735a112ab86a05f6
sprokazin <subtir233@gmail.com>
2022-11-28 20:21:28 +0200

    lab8

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-#Перейдите в ветку, соответствующую номеру лабораторной работы
+import json
+import csv
+import  sys
+
+with open(str(sys.argv[1]), 'r') as file:
+    json_data = json.load(file)
+
+table = []
+for i in (json_data):
+    name = str(i);
+
+table = json_data.pop(name)
+str_table = []
+for i in table:
+    str_table.append(i)
+
+str_file = []
+with open(name + '.csv', 'a+') as file:
+    writer = csv.writer(file)
+    for key, values in str_table[0].items():
+        str_file.append(key)
+        str_file.append(';')
+    str_file.append('\n')
+    for i in range(len(str_table)):
+        for key, value in str_table[i].items():
+            str_file.append(value)
+            str_file.append(';')
+        str_file.append('\n')
+
+    i=0
+    while i<len(str_file):
+        if str_file[i]==';' and str_file[i+1]=='\n':
+            i+=1
+            continue
+        else:
+            file.write(str_file[i])
+            i+=1
+
